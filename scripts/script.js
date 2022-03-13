ac09e2679e19fefd8387c0ca14b028620b6dbf5e
Ran Ren <ranforrestren@gmail.com>
2022-03-13 13:38:06 -0400

    Add player move selection function

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,31 @@
 function computerPlay() {
+  // Generates random number from 1 - 3, and selects move based on number
   let numberPick = Math.floor((Math.random() * (3)) + 1);
-  let movePlayed
+  let computerMove;
   switch(numberPick) {
     case 1:
-      movePlayed = "Rock";
+      computerMove = "rock";
       break;
     case 2:
-      movePlayed = "Paper";
+      computerMove = "paper";
       break;
     case 3:
-      movePlayed = "Scissors";
+      computerMove = "scissors";
       break;
   }
-  return movePlayed;
+  return computerMove;
+}
+
+function playerInput () {
+  // Prompts player for input and checks if move is valid
+  let playerMove = prompt("Please Select Your Move.", "");
+  playerMove = playerMove.toLowerCase();
+  if (playerMove != "rock" && playerMove != "paper" && playerMove != "scissors") 
+  {
+    console.log("Invalid Move!");
+  }
+  return playerMove;
 }
 
 console.log(computerPlay());
+console.log(playerInput());
